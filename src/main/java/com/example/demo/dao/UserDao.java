5f418fb9a0779ff84bb9c8678f33138ae72ecfc5
alen <alen.c@anvo.com>
2018-05-07 16:35:01 +0800

    Spring Boot 整合Mysql Mybatis 测试样例

---
+++ src/main/java/com/example/demo/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by alen.c on 2018/4/24.
+ */
+@Mapper
+public interface UserDao {
+
+    /**
+     * case 1，可以不写com/example/mapper/SmUserMapper.xml和interface的实现类，
+     *      我们可以直接使用@Select注解写sql来直接进行操作数据库，这个就简单了。[
+     *      另外，还有@Insert、@Update、@Delete等注解
+     */
+
+    int insert(User obj);
+
+    User login(String username,String password);
+
+    User findByUsername(String username);
+
+
+}
